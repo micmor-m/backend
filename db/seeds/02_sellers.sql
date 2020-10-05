ca43dd34800cddb8661f3d1036e517a239a8ded1
micmor-m <micmor.m@gmail.com>
2020-10-05 13:06:39 +0000

    update coordinate seeds

--- db/seeds/02_sellers.sql
+++ db/seeds/02_sellers.sql
@@ -3,8 +3,8 @@
 INSERT INTO
   cleaners(username, email, description, address, latitude, longitude, picture_url, phone, password)
 VALUES
-  ('Luigi', 'cleaner@a.com','clean cars and trucks', '11 Front St, Toronto', 44.50, -73.50, 'https://www.pikpng.com/pngl/m/52-526874_luigi-bros-png-luigi-de-mario-bross-clipart.png', '1234567891','test'),
-  ('Mario', 'clean&more@a.com','affordable and fast', '133 University Ave, Toronto', 45.33, -73.20, 'https://www.pikpng.com/pngl/m/30-307327_mario-png-mario-mario-party-9-clipart.png','3334567891','test'),
-  ('Princess Peach', 'queen@a.com','all you need in one place', '124 Lake St, Montreal', 48.10, -73.10, 'https://www.pikpng.com/pngl/m/472-4723258_daisy-and-peach-mario-bros-daisy-saxi-clipart.png', '1234567834', 'test'),
-  ('Princess Daisy', 'king@a.com', 'wash and service', '1 Parlament St, Ottowa',42.30, -73.30, 'https://www.pikpng.com/pngl/m/244-2443348_super-princess-peach-mario-bros-princess-peach-coloring.png', '1234457891', 'test'),
-  ('Donkey Kong', 'donkey@a.com','all cars service', '156 Hill St, Vancouver', 45.70, -73.40, 'https://www.pikpng.com/pngl/m/167-1671773_transparent-donkey-kong-donkey-kong-png-transparent-clipart.png', '2234567891', 'test');
+  ('Luigi', 'luigi@gmail.com','Clean cars and trucks.', '11 Front St, Toronto', 43.642824, -79.393661, 'https://www.pikpng.com/pngl/m/52-526874_luigi-bros-png-luigi-de-mario-bross-clipart.png', '1234567891','test'),
+  ('Mario', 'mario@gmail.com.com','Affordable, reliable and fast.', '133 University Ave, Toronto', 43.654671, -79.388410, 'https://www.pikpng.com/pngl/m/30-307327_mario-png-mario-mario-party-9-clipart.png','3334567891','test'),
+  ('Princess Peach', 'peach@gmail.com','All cleaning services you need.', '124 Lake St, Montreal', 45.445253,  -73.741037, 'https://www.pikpng.com/pngl/m/472-4723258_daisy-and-peach-mario-bros-daisy-saxi-clipart.png', '1234567834', 'test'),
+  ('Princess Daisy', 'daisy@gmail.com', 'Wash and more servicees.', '1 Parlament St, Toronto',43.659533, -79.365630, 'https://www.pikpng.com/pngl/m/244-2443348_super-princess-peach-mario-bros-princess-peach-coloring.png', '1234457891', 'test'),
+  ('Donkey Kong', 'donkey@gmail.com','Master gardening.', '156 Hill St, Toronto', 43.855280, -79.438985, 'https://www.pikpng.com/pngl/m/167-1671773_transparent-donkey-kong-donkey-kong-png-transparent-clipart.png', '2234567891', 'test');
